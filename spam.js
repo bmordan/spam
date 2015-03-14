c3a9c598fa5b7f90a62b74c0ad5dd0ce7ccb2b1d
bmordan <bernardmordan@gmail.com>
2015-03-14 16:32:57 +0000

    basic classic jquery html5

--- spam.js
+++ spam.js
@@ -6,13 +6,21 @@ if (Meteor.isClient) {
       return Spam.find().fetch()
     }
   })
+
+  Template.tick.helpers({
+    tocks: function (id) {
+      var deg = ['rotate(-15deg)','rotate(15deg)']
+      var index = Math.floor((Math.random()*10)+1)%2
+      $('#'+id).css('transform',deg[index])
+    }
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     Spam.remove({})
     Meteor.setInterval(function add () {
-      Spam.insert({ts: moment().format('ss')})
-    },1000)
+      Spam.insert({ts: moment().format('s.SS')})
+    },600)
   });
 }
