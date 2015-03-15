b18b9eb91dc6e3f875f3a6979d11f2f441b508a3
bmordan <bernardmordan@gmail.com>
2015-03-15 15:18:01 +0000

    initial physics with simple force

--- client/famous/famous.js
+++ client/famous/famous.js
@@ -1,13 +1,30 @@
 var context = famous.core.Engine.createContext()
+var physics = new famous.physics.PhysicsEngine()
+var centerPin = new famous.physics.bodies.Particle({
+  position: [0,0,0]
+})
+physics.addBody(centerPin)
 
 Template.surface.rendered = function () {
   var surface = new famous.core.Surface({
     size: [50,50],
     classes: ['spam']
   })
+  var particle = new famous.physics.bodies.Particle({
+    position: [0,0,0]
+  })
   var modifier = new famous.core.Modifier({
     origin: [0.5,0.5],
-    align: [0.5,0.5]
+    align: [0.5,0.5],
+    transform: function () {
+      return particle.getTransform()
+    }
   })
+  var force = new famous.physics.forces.Repulsion({
+    strength: 2
+  })
+  physics.addBody(particle)
+  physics.attach(force, centerPin, particle)
+  particle.setVelocity([0.1,0,0])
   context.add(modifier).add(surface)
 }
