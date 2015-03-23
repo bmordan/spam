53761dc62d242ab3a159e014cfe21ac33d52e3e3
bmordan <bernardmordan@gmail.com>
2015-03-23 22:48:54 +0000

    adds walls

--- client/classic/classic.js
+++
@@ -1,7 +0,0 @@
-Template.tick.helpers({
-  tocks: function (id) {
-    var deg = ['rotate(-15deg)','rotate(15deg)']
-    var index = Math.floor((Math.random()*10)+1)%2
-    $('#'+id).css('transform',deg[index])
-  }
-})
--- client/famous/famous.js
+++ client/famous/famous.js
@@ -3,8 +3,6 @@ var physics = new famous.physics.PhysicsEngine()
 var centerPin = new famous.physics.bodies.Particle({
   position: [0,0,0]
 })
-var collision = new famous.physics.constraints.Collision()
-var Walls = famous.physics.constraints.Walls
 
 physics.addBody(centerPin)
 
@@ -14,11 +12,11 @@ Template.surface.rendered = function () {
     classes: ['spam']
   })
   var particle = new famous.physics.bodies.Particle({
-    position: [0,0,0]
+    position: [0.1,0.001,0]
   })
   var modifier = new famous.core.Modifier({
     origin: [0.5,0.5],
-    align: [0,0.5],
+    align: [0,0],
     transform: function () {
       return particle.getTransform()
     }
@@ -26,27 +24,21 @@ Template.surface.rendered = function () {
   var force = new famous.physics.forces.Repulsion({
     strength: 0.05
   })
-
-  var widthHeight = context.getSize()
-  var walls = new Walls({
-    restitution: 2,
-    origin: [0.5,0.5],
-    align: [0.5,0.5],
-    size: [widthHeight[0],widthHeight[1]],
-    sides: famous.physics.constraints.Walls.SIDES.TWO_DIMENSIONAL
+  var rightWall = new famous.physics.constraints.Wall({
+    normal: [-1,0,0],
+    distance: context.getSize()[0],
+    restituition: 0.2
+  })
+  var leftWall = new famous.physics.constraints.Wall({
+    normal: [1,0,0],
+    distance: 0,
+    restituition: 0.2
   })
 
   physics.addBody(particle)
   physics.attach(force, particle, centerPin)
-  // physics.attach(
-  //   collision,
-  //   particle,
-  //   walls.components[0],
-  //   walls.components[1],
-  //   walls.components[2],
-  //   walls.components[3]
-  // )
+  physics.attach([rightWall,leftWall], particle)
 
-  particle.setVelocity(0.01,0.01,0.01)
+  particle.setVelocity(0.01,0.01,0)
   context.add(modifier).add(surface)
 }
